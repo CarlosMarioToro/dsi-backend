3daa915edf49ff296196cfa0bd67711fe9c49920
CarlosMarioToro <ctoroz59@gmail.com>
2024-05-07 20:33:43 -0500

    CreandoIndex y Variables de entorno

--- src/config/db.js
+++ src/config/db.js
@@ -1,37 +1,15 @@
-import sql from 'mssql';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const dbSettings = {
-    server: process.env.DB_SERVER,
-    authentication: {
-        type: 'default',
-        options: {
-            userName: process.env.DB_USER,
-            password: process.env.DB_PASS,
-        }
-    },
-    options: {
-        port: 1433,
-        database: process.env.DB_DATABASE,
-        trustServerCertificate: true
-    }
-}
+import mongoose from "mongoose";
 
 const conectarDB = async () => {
     try {
-        const pool = await sql.connect(dbSettings, err => {
-            if (err) console.log(err)
-            console.log("Connection Successful!");
-        });
-        // const url = `${pool.config.server}:${pool.config.port}`;
-        // console.log(`Sql Server conectado en ${url}`);
-        return pool;
+        const db = await mongoose.connect(process.env.MONGO_URI);
+        
+        const url = `${db.connection.host}:${db.connection.port}`;
+        console.log(`MongoDB conectado en: ${url}`);
     } catch (error) {
         console.log(`error: ${error.message}`);
         process.exit(1);
     }
-}
+};
 
 export default conectarDB;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import express from 'express'
 import conectarDB from './config/db.js';
 import dotenv from 'dotenv';
-import usuariosRoutes from  './routes/usuarios.routes.js';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+// import usuariosRoutes from  './routes/usuarios.routes.js';
+// import cors from 'cors';
+// import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 conectarDB();
 
-app.use(cors());
+// app.use(cors());
 
-app.use('/dsi', usuariosRoutes)
+// app.use('/dsi', usuariosRoutes)
 
 const PORT = process.env.PORT || 4000;
 
